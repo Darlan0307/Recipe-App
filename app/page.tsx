0acf9e27f21ebc62acb0c5b3a2fda8830ed46974
Darlan0307 <darlanchagas2020@gmail.com>
2024-05-07 19:30:49 -0300

    sparkles: feat: componente button do shadcnui:

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>home page</h1>;
+  return (
+    <>
+      <h1>home page</h1>
+      <Button>teste</Button>
+    </>
+  );
 }
