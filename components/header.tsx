cb5bded26c91a786f9027640894f7816a3bcc0c7
Darlan0307 <darlanchagas2020@gmail.com>
2024-05-09 19:45:16 -0300

    feat: estrutura inicial do header da aplicação

--- components/header.tsx
+++ components/header.tsx
@@ -2,17 +2,23 @@ import Image from "next/image";
 import Link from "next/link";
 import MenuMobile from "./menu-mobile";
 import Search from "./search";
+import MenuDesktop from "./menu-desktop";
 
 const Header = () => {
   return (
-    <header className="grid grid-cols-2 items-center gap-6 p-5 sm:grid-cols-3">
+    <header className="grid grid-cols-2 items-center gap-6 p-5 sm:grid-cols-3 lg:flex lg:justify-between lg:px-12">
       <div className="relative w-[100px] h-[100px]">
         <Link href="/">
           <Image src="/logo.png" alt="website logo" fill sizes="100%" />
         </Link>
       </div>
-      <div className="justify-self-end sm:col-start-3">
-        <MenuMobile />
+      <div className="justify-self-end sm:col-start-3 lg:order-3">
+        <div className="lg:hidden">
+          <MenuMobile />
+        </div>
+        <div className="hidden lg:block">
+          <MenuDesktop />
+        </div>
       </div>
       <div className="col-span-2 justify-self-center min-w-[300px] w-fill max-w-[400px] sm:col-span-1 sm:col-start-2 sm:row-start-1">
         <Search />
--- components/menu-mobile.tsx
+++ components/menu-mobile.tsx
@@ -1,13 +1,14 @@
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import { Separator } from "./ui/separator";
 
 const MenuMobile = () => {
   return (
@@ -17,14 +18,28 @@ const MenuMobile = () => {
           <Menu size={30} />
         </Button>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="flex flex-col justify-start gap-8">
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
+          <SheetTitle>Seja bem-vindo(a)!</SheetTitle>
         </SheetHeader>
+        <div className="flex items-center justify-center gap-4 ">
+          <Button>Login</Button>
+          <Button>Toggle theme</Button>
+        </div>
+        <Separator className=" bg-muted-foreground" />
+        <nav className="flex items-center gap-5 flex-col ">
+          <Link href="#" className="font-semibold hover:underline">
+            Home
+          </Link>
+          <Link href="#" className="font-semibold hover:underline">
+            Receitas
+          </Link>
+          <Link href="#" className="font-semibold hover:underline">
+            Análise
+          </Link>
+        </nav>
+        <Separator className=" bg-muted-foreground" />
+        <p>outras opções</p>
       </SheetContent>
     </Sheet>
   );
