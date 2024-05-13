9ea8e2d07b0ab7dd7c08ba0373e679a977f26c45
Darlan0307 <darlanchagas2020@gmail.com>
2024-05-13 18:38:04 -0300

    feat: estruturando a pagina das receitas

---
+++ app/recipes/_actions/fetch-recipes.ts
@@ -0,0 +1,16 @@
+"use server";
+import { RecipeType } from "@/app/@types/RecipeType";
+
+export const FetchRecipesRandom = async (): Promise<RecipeType[]> => {
+  const fetchedRecipes: RecipeType[] = [];
+  for (let i = 0; i < 10; i++) {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const data = await response.json();
+    const dataRecipe = (await data.meals[0]) as RecipeType;
+    fetchedRecipes.push(dataRecipe);
+  }
+  console.log("fez a requisição");
+  return fetchedRecipes;
+};
--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,10 +1,29 @@
-import Header from "@/components/header";
+"use client";
 
-const Recipes = async () => {
+import Header from "@/components/header";
+import { FetchRecipesRandom } from "./_actions/fetch-recipes";
+import { useEffect, useState } from "react";
+import { RecipeType } from "../@types/RecipeType";
+
+const Recipes = () => {
+  const [data, setData] = useState<RecipeType[]>([]);
+
+  const FetchData = async () => {
+    const response = await FetchRecipesRandom();
+
+    setData(response);
+  };
+
+  useEffect(() => {
+    FetchData();
+  }, []);
+
   return (
     <div>
       <Header />
       <p>page recipes</p>
+      {data &&
+        data.map((recipe) => <p key={crypto.randomUUID()}>{recipe.strArea}</p>)}
     </div>
   );
 };
