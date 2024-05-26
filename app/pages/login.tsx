c5c3e53bcbff77eda81ae0e1ac302055d84504b8
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2024-05-26 21:28:08 +0000

    Pending changes exported from your codespace

---
+++ app/pages/login.tsx
@@ -0,0 +1,14 @@
+// pages/login.tsx
+import React from 'react';
+import Login from '../../components/Login';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Log in to your account</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
