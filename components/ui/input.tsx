88bbe0007a2cbb0b0570472ac451dae631fdf0a4
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2024-05-26 18:28:17 -0700

    feat: ⏎ Return settings shadcn ui

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,7 +1,9 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
+import * as React from "react"
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+import { cn } from "@/lib/utils"
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -15,10 +17,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    );
+    )
   }
-);
+)
+Input.displayName = "Input"
 
-Input.displayName = "Input";
-
-export { Input };
+export { Input }
