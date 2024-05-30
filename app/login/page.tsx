9a1b5d512b58e35cfabd5d7242e7a606d8596f9e
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2024-05-30 12:12:17 -0700

    feat: 🔐 login page design

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,14 @@
-// pages/Recipes.tsx
 import React from 'react';
 import Header from '@/components/Header';
-import Login1 from '@/components/Login';
+import LoginForm from '@/components/Login';
 
-function Login() {
+export default function LoginPage() {
   return (
     <div>
       <Header />
-      <div className="text-center mt-8">
-        <h1 className="text-3xl font-bold mb-4">Login</h1>
-        <p className="text-gray-600">Welcome to the login page!</p>
-      </div>
       <div className="container mx-auto mt-8">
-        <Login1 />
+        <LoginForm />
       </div>
     </div>
   );
 }
-
-export default Login;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-// pages/Home.tsx
 import React from 'react';
 import Header from "@/components/Header";
-import Link from 'next/link';
 import HeroSection from '@/components/HeroSection';
-import Login from '@/components/Login';
 
 export default function Home() {
   return (
@@ -12,12 +9,7 @@ export default function Home() {
         <Header />
       </div>
       <div>
-        <Link href="/">
-          <HeroSection />
-        </Link>
-        <Link href="/login">
-          <Login />
-        </Link>
+        <HeroSection />
       </div>
     </>
   );
