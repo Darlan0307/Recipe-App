04a143087539ee5e916b3307e071876d29431711
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2024-07-03 17:12:32 +0000

    feat: button pagination

--- app/recipes/_components/button-pagination.tsx
+++ app/recipes/_components/button-pagination.tsx
@@ -15,22 +15,23 @@ const ButtonPagination: React.FC<ButtonPaginationProps> = ({ currentPage, totalP
     router.push(`/recipes?page=${page}`);
   };
 
+  
   return (
     <div className="flex justify-center space-x-4 mt-6">
       <button
         onClick={() => handlePageChange(Math.max(currentPage - 1, 1))}
         disabled={currentPage === 1}
-        className="px-4 py-2 border rounded bg-blue-500 text-white disabled:bg-gray-400"
+        className="group text-white bg-gradient-to-r from-red-400 via-red-500 to-orange-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex flex-col items-center h-[50px] w-[70px]"
       >
         Previous
       </button>
       <span>
-        Page {currentPage} of {totalPages}
+      📑 {currentPage} of {totalPages}
       </span>
       <button
         onClick={() => handlePageChange(Math.min(currentPage + 1, totalPages))}
         disabled={currentPage === totalPages}
-        className="px-4 py-2 border rounded bg-blue-500 text-white disabled:bg-gray-400"
+        className="group text-white bg-gradient-to-r from-red-400 via-red-500 to-orange-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex flex-col items-center h-[50px] w-[70px]"
       >
         Next
       </button>
