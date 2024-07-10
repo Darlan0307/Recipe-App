4336226a76fa42c012d0bd4fbfa4e93e577558a4
Darlan0307 <darlanchagas2020@gmail.com>
2024-07-09 21:06:55 -0300

    fix: resolvendo bug na tipagem do children no arquivo layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Providers from "@/providers/providers";
+import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,17 +12,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: ReactNode;
+  types: ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
         <script async src="https://cdn.lordicon.com/lordicon.js"></script>
       </head>
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
